63175bd37ffa9e5a0dc405e7fce6d5884221cd94
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-05-31 11:26:27 -0700

    qa-gitflow: update desc

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "aws_instance" "ubuntu" {
     Name        = "${var.name}"
     TTL         = "${var.ttl}"
     Owner       = "${var.owner}"
-    Description = "This is a tremendous description"
+    Description = "This is a super tremendous description"
   }
 }
