30ba5fa4c40d2fe9d590266421f26b5f38e9fe52
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-06-05 20:12:06 -0700

    qa-gitflow: adding in next levels to see what happens

---
+++ one_level/two_level/three_level/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_instance" "ubuntu" {
+  ami               = "${var.ami_id}"
+  instance_type     = "${var.instance_type}"
+  availability_zone = "${var.aws_region}a"
+
+  tags {
+    Name        = "${var.name}"
+    TTL         = "${var.ttl}"
+    Owner       = "${var.owner}"
+    Description = "This is a tremendous description"
+  }
+}
