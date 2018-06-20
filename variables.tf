d66e0d806126766d0f264c416a916c4b485a993b
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-06-20 12:00:47 -0700

    qa-gitflow: add in flow

--- variables.tf
+++ variables.tf
@@ -1,8 +1,12 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "us-east-1"
 }
 
+variable "aws_profile" {
+  description = "AWS profile used for provider connection"
+  default = "default"
+}
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
