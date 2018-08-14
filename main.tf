4e07885bfa5ad494fde6045cccfd12770b0f803a
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-08-14 11:08:46 -0700

    qa-gitflow: adding descriptor

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "aws_instance" "ubuntu" {
     Name        = "${var.name}"
     TTL         = "${var.ttl}"
     Owner       = "${var.owner}"
-    Description = "This is a demo description"
+    Description = "This is a demo qa description"
   }
 }
